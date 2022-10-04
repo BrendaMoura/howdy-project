6736f83280a0396850fd29dd2a58f5539d1d57c7
Pedro Henrique <2020002900@ifam.edu.br>
2022-10-03 21:20:36 -0400

    first commit

---
+++ app/src/main/java/com/example/projetohowdy/FormLogin.java
@@ -0,0 +1,13 @@
+package com.example.projetohowdy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
