8396af8fbd169547863a55d479edb8240eebc56e
Brenda Moura <brendamoura.bcdm@gmail.com>
2022-10-10 19:02:36 -0400

    Adding delete account function

--- app/src/main/java/com/example/projetohowdy/controller/UserController.java
+++ app/src/main/java/com/example/projetohowdy/controller/UserController.java
@@ -20,47 +20,20 @@ public class UserController {
     private FirebaseAuth auth;
     private String finalMessage = "No response yet";
 
-    public String createUser (User user) {
-        auth = FirebaseConfiguration.getFirebaseAuth();
+    public String deleteUser () {
+        try{
+            auth = FirebaseConfiguration.getFirebaseAuth();
 
-        auth.createUserWithEmailAndPassword(user.email.toLowerCase(), user.password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    String encryptedPassword = Encryption.encode64(user.getPassword());
-                    user.setPassword(encryptedPassword);
+            DocumentReference documentReference = FirebaseConfiguration.getFirebaseFirestore().collection("Users").document(auth.getCurrentUser().getUid());
+            documentReference.delete();
 
-                    DocumentReference documentReference = FirebaseConfiguration.getFirebaseFirestore().collection("Users").document(auth.getCurrentUser().getUid());
-                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            System.out.println("Success");
-                            finalMessage = "Sucesso ao cadastrar!";
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            finalMessage = "Erro ao cadastrar!";
-                        }
-                    });
-                }else{
-                    try {
-                        throw task.getException();
-                    }catch (FirebaseAuthWeakPasswordException e){
-                        finalMessage = "Por favor, tente uma senha mais forte!";
-                    }catch (FirebaseAuthInvalidCredentialsException e){
-                        finalMessage = "Email inválido!";
-                    }catch (FirebaseAuthUserCollisionException e){
-                        finalMessage = "Conta já cadastrada!";
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        finalMessage = "Erro ao cadastrar!";
-                    }
-                }
-            }
-        });
+            auth.getCurrentUser().delete();
+
+            finalMessage = "Conta excluída com sucesso!";
+        }catch (Exception e){
+            finalMessage = "Erro ao excluir conta!";
+        }
 
         return finalMessage;
     }
-
 }
