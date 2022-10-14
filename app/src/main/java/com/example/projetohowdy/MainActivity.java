ce8f807e0bff7c36fac03f1192e5078dfe8481ff
Brenda Moura <brendamoura.bcdm@gmail.com>
2022-10-14 10:56:48 -0400

    Adding connect and disconnect functions

--- app/src/main/java/com/example/projetohowdy/MainActivity.java
+++ app/src/main/java/com/example/projetohowdy/MainActivity.java
@@ -4,11 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.projetohowdy.controller.UserController;
-import com.example.projetohowdy.model.User;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.ktx.Firebase;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/projetohowdy/controller/utils/FirebaseConfiguration.java
+++ app/src/main/java/com/example/projetohowdy/controller/utils/FirebaseConfiguration.java
@@ -1,11 +1,17 @@
 package com.example.projetohowdy.controller.utils;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FirebaseConfiguration {
     private static FirebaseAuth auth;
     private static FirebaseFirestore db;
+    private static boolean result = false;
 
     //return FirebaseAuth instance
     //authentication
@@ -22,6 +28,30 @@ public class FirebaseConfiguration {
             db = FirebaseFirestore.getInstance();
         }
         return db;
+    }
+
+    public boolean connectUser(String email, String senha){
+
+        getFirebaseAuth().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    result = true;
+                }
+            }
+        });
+
+        return result;
+    }
+
+    public boolean disconnectUser(){
+        try{
+            auth.signOut();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
