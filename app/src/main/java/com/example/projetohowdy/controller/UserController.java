645dc310d276f8ea2e80ea1d16505daa9e62e6d9
Brenda Moura <brendamoura.bcdm@gmail.com>
2022-10-14 20:42:05 -0400

    Adding update function

--- app/src/main/java/com/example/projetohowdy/controller/UserController.java
+++ app/src/main/java/com/example/projetohowdy/controller/UserController.java
@@ -20,47 +20,33 @@ public class UserController {
     private FirebaseAuth auth;
     private String finalMessage = "No response yet";
 
-    public String createUser (User user) {
+    public String updateAccount(User user){
         auth = FirebaseConfiguration.getFirebaseAuth();
+        String email = auth.getCurrentUser().getEmail();
 
-        auth.createUserWithEmailAndPassword(user.email.toLowerCase(), user.password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        DocumentReference documentReference = FirebaseConfiguration.getFirebaseFirestore().collection("Users").document(auth.getCurrentUser().getUid());
+        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    String encryptedPassword = Encryption.encode64(user.getPassword());
-                    user.setPassword(encryptedPassword);
-
-                    DocumentReference documentReference = FirebaseConfiguration.getFirebaseFirestore().collection("Users").document(auth.getCurrentUser().getUid());
-                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            System.out.println("Success");
-                            finalMessage = "Sucesso ao cadastrar!";
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            finalMessage = "Erro ao cadastrar!";
-                        }
-                    });
-                }else{
-                    try {
-                        throw task.getException();
-                    }catch (FirebaseAuthWeakPasswordException e){
-                        finalMessage = "Por favor, tente uma senha mais forte!";
-                    }catch (FirebaseAuthInvalidCredentialsException e){
-                        finalMessage = "Email inválido!";
-                    }catch (FirebaseAuthUserCollisionException e){
-                        finalMessage = "Conta já cadastrada!";
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        finalMessage = "Erro ao cadastrar!";
+            public void onSuccess(Void unused) {
+                try{
+                    if( email != user.email){
+                        auth.getCurrentUser().updateEmail(user.email);
                     }
+
+                    auth.getCurrentUser().updatePassword(user.password);
+                    finalMessage = "Sucesso ao alterar conta!";
+                }catch (Exception e){
+                    e.printStackTrace();
+                    finalMessage = "Erro ao alterar conta!";
                 }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                finalMessage = "Erro ao alterar conta!";
             }
         });
 
         return finalMessage;
     }
-
 }
