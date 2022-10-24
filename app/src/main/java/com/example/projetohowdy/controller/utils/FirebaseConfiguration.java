8e65d4dce510a0fae60a4a3e726741053743dd84
brenda.moura <brenda.moura@Plugifys-MacBook-Pro.local>
2022-10-24 16:36:53 -0400

    New functions

--- app/src/main/java/com/example/projetohowdy/controller/utils/FirebaseConfiguration.java
+++ app/src/main/java/com/example/projetohowdy/controller/utils/FirebaseConfiguration.java
@@ -7,21 +7,46 @@ public class FirebaseConfiguration {
     private static FirebaseAuth auth;
     private static FirebaseFirestore db;
 
-    //return FirebaseAuth instance
-    //authentication
+    // return FirebaseAuth instance
+    // authentication
     public static FirebaseAuth getFirebaseAuth() {
-        if(auth == null){
+        if (auth == null) {
             auth = FirebaseAuth.getInstance();
         }
         return auth;
     }
 
-    //return Firebase Firestore instance
-    public static FirebaseFirestore getFirebaseFirestore(){
-        if(db == null){
+    // return Firebase Firestore instance
+    public static FirebaseFirestore getFirebaseFirestore() {
+        if (db == null) {
             db = FirebaseFirestore.getInstance();
         }
         return db;
+    }
+
+    public boolean connectUser(String email, String senha) {
+
+        getFirebaseAuth().signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            result = true;
+                        }
+                    }
+                });
+
+        return result;
+    }
+
+    public boolean disconnectUser() {
+        try {
+            auth.signOut();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
