bcd7a115b9ca7f6ef9cd7d9adf9d053a7d27cd5a
brenda.moura <brenda.moura@Plugifys-MacBook-Pro.local>
2022-11-11 22:37:26 -0400

    Login e cadastro

--- app/src/main/java/com/example/projetohowdy/FormCadastro.java
+++ app/src/main/java/com/example/projetohowdy/FormCadastro.java
@@ -1,14 +1,94 @@
 package com.example.projetohowdy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.projetohowdy.controller.UserController;
+import com.example.projetohowdy.controller.utils.Encryption;
+import com.example.projetohowdy.controller.utils.FirebaseConfiguration;
+import com.example.projetohowdy.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
 
 public class FormCadastro extends AppCompatActivity {
+    TextView name, user, email, password;
+    Button cadastrar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        name = findViewById(R.id.cadastroname);
+        // user = findViewById(R.id.cadastrouser);
+        email = findViewById(R.id.cadastroemail);
+        password = findViewById(R.id.cadastrosenha);
+        cadastrar = findViewById(R.id.cadastrar);
+
+        acao();
+    }
+
+    public void acao(){
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User newUser = new User("teste", name.getText().toString(), email.getText().toString(), password.getText().toString());
+
+                auth = FirebaseConfiguration.getFirebaseAuth();
+                auth.createUserWithEmailAndPassword(newUser.getEmail().toLowerCase(), newUser.getPassword()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String encryptedPassword = Encryption.encode64(newUser.getPassword());
+                            newUser.setPassword(encryptedPassword);
+
+                            DocumentReference documentReference = FirebaseConfiguration.getFirebaseFirestore().collection("Users").document(auth.getCurrentUser().getUid());
+                            documentReference.set(newUser).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Intent intent = new Intent(FormCadastro.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    //Adicionar mensagem de falha
+                                    Toast.makeText(FormCadastro.this, "Erro ao cadastrar novo usuário!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                Toast.makeText(FormCadastro.this, "Por favor, tente uma senha mais forte!", Toast.LENGTH_SHORT).show();
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                Toast.makeText(FormCadastro.this, "Email inválido!", Toast.LENGTH_SHORT).show();
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                Toast.makeText(FormCadastro.this, "Conta já cadastrada!", Toast.LENGTH_SHORT).show();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(FormCadastro.this, "Erro ao cadastrar!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projetohowdy/FormLogin.java
+++ app/src/main/java/com/example/projetohowdy/FormLogin.java
@@ -2,13 +2,47 @@ package com.example.projetohowdy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.projetohowdy.controller.utils.FirebaseConfiguration;
 
 public class FormLogin extends AppCompatActivity {
+    TextView email, senha, cadastro;
+    Button entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+
+        email = findViewById(R.id.cadastroemail);
+        senha = findViewById(R.id.cadastrosenha);
+        entrar = findViewById(R.id.login);
+        cadastro = findViewById(R.id.cadastro);
+
+        acao();
+
+    }
+
+    public void acao(){
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseConfiguration firebaseConfiguration = new FirebaseConfiguration();
+                firebaseConfiguration.connectUser(email.getText().toString(), senha.getText().toString());
+            }
+        });
+
+        cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
